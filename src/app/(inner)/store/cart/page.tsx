d94aa42d438bbf2c900b697f2be63c7b8ab44a89
Dike Wisdom <dikewisdom787@gmail.com>
2024-10-03 13:56:26 +0100

    Refactor cart page and cart component

--- src/app/(inner)/store/cart/page.tsx
+++ src/app/(inner)/store/cart/page.tsx
@@ -1,7 +1,19 @@
+import CartComponent from "@/components/route_component/cart-component";
+
 const page = () => {
     return (
-        <div>
-            Enter
+        <div className="relative p-2 md:p-5">
+            <div className="flex items-center justify-between mb-3">
+                <h1 className="md:text-lg font-bold mb-3">
+                    Cart
+                </h1>
+                <button 
+                className="bg-primary-dark-pink hover:bg-primary-text-dark-pink duration-200 text-white text-sm font-bold py-2 px-4 rounded-lg"
+                >   
+                 Checkout
+                </button>
+            </div>
+            <CartComponent />
         </div>
     );
 }
