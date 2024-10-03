ca19d99c0be124d552932b9a79003d974553b230
Dike Wisdom <dikewisdom787@gmail.com>
2024-10-03 16:32:59 +0100

    Refactor cart page and cart component, and add link to continue shopping

--- src/app/(inner)/store/cart/checkout/page.tsx
+++ src/app/(inner)/store/cart/checkout/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { LucideArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
+
+
 const page = () => {
+     const [paymentMethod, setPaymentMethod] = useState<"points" | "paystack">("points");
      return (
           <div className="relative p-2 md:p-5">
-               <h2 className="font-bold text-sm text-gray-600 mb-3">
-                    Customer Details
-               </h2>
+               <div className="flex items-center justify-between mb-3">
+                    <h2 className="font-bold text-sm text-gray-600">
+                         Customer Details
+                    </h2>
+                    <Link href="/store/cart" className="text-primary-dark-pink hover:text-primary-text-dark-pink duration-200 text-xs font-semibold flex items-center">
+                         <LucideArrowLeft size={15} /> Back to Cart
+                    </Link>
+               </div>
                <div>
                     <form action="" className="grid grid-cols-2 border p-2 gap-3 rounded mb-5">
                          <div className="col-span-2">
@@ -76,19 +90,23 @@ const page = () => {
                </h2>
                <div className="grid grid-cols-2 gap-3 rounded mb-3 py-2">
                     <div className="col-span-1">
-                         <button className="border-gray-300 border rounded outline-none p-3 text-xs w-full hover:outline-2 hover:outline-primary-dark-pink">
+                         <button
+                              onClick={() => setPaymentMethod("points")}
+                              className={`border-gray-300 border rounded outline-none p-3 text-xs w-full hover:outline-2 ${paymentMethod === "points" && "outline-primary-dark-pink bg-primary-dark-pink text-white"}`}>
                               Pay With Points
                          </button>
                     </div>
                     <div className="col-span-1">
-                         <button className="border-gray-300 border rounded outline-none p-3 text-xs w-full hover:outline-2 hover:outline-primary-dark-pink">
+                         <button
+                              onClick={() => setPaymentMethod("paystack")}
+                              className={`border-gray-300 border rounded outline-none p-3 text-xs w-full hover:outline-2 ${paymentMethod === "paystack" && "outline-primary-dark-pink bg-primary-dark-pink text-white"}`}>
                               Pay With Card/Bank/Transfer
                          </button>
                     </div>
                </div>
                <div className="col-span-2">
                     <button
-                         className="bg-primary-dark-pink text-white rounded p-2 text-xs w-full">
+                         className="bg-primary-dark-pink text-white rounded p-3 font-bold text-xs w-full">
                          Pay Now
                     </button>
                </div>
