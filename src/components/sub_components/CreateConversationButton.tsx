d0bfa45511872c4f5c5103e09c227cf977cb0973
= <=>
2025-06-02 22:35:36 +0100

    feat: Add loading state and spinner to CreateConversationButton for better user feedback

--- src/components/sub_components/CreateConversationButton.tsx
+++ src/components/sub_components/CreateConversationButton.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useUserAuthContext } from "@/lib/UserUseContext";
 import { createNewConversation } from "@/utils/data/CreateConversation";
-import { LucideMail } from "lucide-react";
+import { LucideLoader2, LucideMail } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 export default function CreateConversationButton({
@@ -11,29 +12,32 @@ export default function CreateConversationButton({
   profileId: string;
 }) {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const { user } = useUserAuthContext();
   const createConversation = async () => {
-    toast.loading("");
+    setLoading(true);
     createNewConversation({ userId: user?.user_id as string, profileId })
       .then((res) => {
         if (res?.status === 200 && res.data.status === true) {
           router.push(`/chats/${res.data.conversation_id}`);
-          toast.dismiss();
+          setLoading(false);
         } else {
-          toast.dismiss();
           toast.error("sorry you cant message this user at the moment");
+          setLoading(false);
         }
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   };
   return (
     <button
       onClick={createConversation}
-      className="p-1 text-white cursor-pointer rounded bg-primary-dark-pink"
+      className="p-1 text-white cursor-pointer rounded bg-primary-dark-pink flex items-center"
     >
       <LucideMail className="w-5 h-5" />
+      {loading && <LucideLoader2 className="animate-spin" />}
     </button>
   );
 }
