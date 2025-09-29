56ecd124dba6eb2bba365bc5543c5f44901db004
= <=>
2025-09-29 17:12:45 +0100

    feat: Update Messages link icon to LucideMail and integrate unread message count in NavigationBar

--- src/components/modals/MenuButtons.tsx
+++ src/components/modals/MenuButtons.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {
   LucideHome,
+  LucideMail,
   LucidePlus,
   LucideSearch,
   LucideUser2,
@@ -10,6 +11,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useAuthContext } from "@/contexts/UserUseContext";
 import { cn } from "../ui/cn";
+import { useMessagesConversation } from "@/contexts/MessageConversationContext";
 
 const MenuButtons = () => {
   const { isGuest } = useAuthContext();
@@ -27,12 +29,13 @@ const MenuButtons = () => {
 };
 
 const NavigationBar = () => {
+  const { unreadCount } = useMessagesConversation();
   const navigationLinks = [
     { name: "Home", href: "/", icon: LucideHome },
     { name: "Search", href: "/search", icon: LucideSearch },
     { name: "New Post", href: "/new", icon: LucidePlus },
     { name: "Models", href: "/models", icon: LucideUser2 },
-    { name: "Messages", href: "/messages", icon: MessageCircle },
+    { name: "Messages", href: "/messages", icon: LucideMail },
   ];
   const pathname = usePathname();
   return (
@@ -42,14 +45,21 @@ const NavigationBar = () => {
           <Link
             key={link.name}
             href={link.href}
-            className="cursor-pointer flex-1 w-full text-center  py-4 md:py-8 px-8 md:px-16 "
+            className="cursor-pointer relative flex-1 w-full text-center  py-4 md:py-8 px-8 md:px-16 "
           >
-            <link.icon
-              className={cn("h-5 md:h-8", {
-                "stroke-black": pathname !== link.href,
-                "stroke-primary-dark-pink": pathname === link.href,
-              })}
-            />
+            <span className="relative inline-block">
+              <link.icon
+                className={cn("h-5 md:h-8", {
+                  "stroke-black": pathname !== link.href,
+                  "stroke-primary-dark-pink": pathname === link.href,
+                })}
+              />
+              {link.name === "Messages" && (
+                <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">
+                  {unreadCount}
+                </span>
+              )}
+            </span>
           </Link>
         ))}
       </div>
