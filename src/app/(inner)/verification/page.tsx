69ac4f3bddf27cc0aa1afa0ea8663f45e2c2c248
= <=>
2025-10-30 12:49:11 +0100

    Fixed Verificatio Page X Overflow

--- src/app/(inner)/verification/page.tsx
+++ src/app/(inner)/verification/page.tsx
@@ -9,68 +9,72 @@ import VerificationPageButton from "@/features/verification/VerificationButton";
 import getUserData from "@/utils/data/UserData";
 
 const Verification = () => {
-  const {
-    data: user,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["userProfileData"],
-    queryFn: getUserData,
-  });
+    const {
+        data: user,
+        isLoading,
+        error,
+    } = useQuery({
+        queryKey: ["userProfileData"],
+        queryFn: getUserData,
+    });
 
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
+    if (isLoading) {
+        return <LoadingSpinner />;
+    }
 
-  if (error || !user) {
-    return <div className="p-4">Error loading user data.</div>;
-  }
+    if (error || !user) {
+        return <div className="p-4">Error loading user data.</div>;
+    }
 
-  if (user?.Model?.verification_status || !user?.is_model) {
-    redirect("/profile");
-  }
+    if (user?.Model?.verification_status || !user?.is_model) {
+        redirect("/profile");
+    }
 
-  return (
-    <div className="p-5 min-h-dvh md:p-7 dark:text-white">
-      <div className="pt-12 mx-auto mb-24 max-w-screen-xl md:mt-16">
-        <h1 className="mt-auto mb-16 text-2xl font-bold text-center">
-          Verify Yourself
-        </h1>
-        <div className="mx-auto mb-12 grid max-w-96">
-          <div>
-            <Image
-              width={150}
-              height={150}
-              src="/images/verification.png"
-              alt=""
-              className="block mx-auto mb-4 text-center aspect-square rounded-xl"
-            />
-          </div>
+    return (
+        <div className="p-5 min-h-dvh md:p-7 dark:text-white">
+            <div className="pt-12 mx-auto mb-24 max-w-screen-xl md:mt-16">
+                <h1 className="mt-auto mb-16 text-2xl font-bold text-center">
+                    Verify Yourself
+                </h1>
+                <div className="mx-auto mb-12 grid max-w-96">
+                    <div>
+                        <Image
+                            width={150}
+                            height={150}
+                            src="/images/verification.png"
+                            alt=""
+                            className="block mx-auto mb-4 text-center aspect-square rounded-xl"
+                        />
+                    </div>
+                </div>
+                <h5 className="mx-auto mb-4 text-lg font-bold text-center md:max-w-96">
+                    Verify your identity by taking a short video
+                </h5>
+                <p className="mx-auto mb-6 text-sm leading-relaxed text-center max-w-96">
+                    Help us keep Paymefans safe by verifying your identity. This
+                    lets us know you&apos;re a real person and helps us keep
+                    your account secure.
+                    <br />
+                    <br />
+                    We&apos;ll keep your information private and delete it after
+                    we&apos;ve confirmed your identity.
+                </p>
+                <div className="flex items-center p-4 py-4 mx-auto bg-coins-card-bottom max-w-96 rounded-xl gap-4">
+                    <span>
+                        <LucideInfo
+                            className="text-primary-text-dark-pink"
+                            size={30}
+                        />
+                    </span>
+                    <p className="text-sm font-medium text-primary-text-dark-pink">
+                        Note that these photos will not appear on your profile
+                        and are only for verification purposes.
+                    </p>
+                </div>
+                <VerificationPageButton />
+            </div>
         </div>
-        <h5 className="mx-auto mb-4 text-lg font-bold text-center md:max-w-96">
-          Verify your identity by taking a short video
-        </h5>
-        <p className="mx-auto mb-6 text-sm leading-relaxed text-center max-w-96">
-          Help us keep Paymefans safe by verifying your identity. This lets us
-          know you&apos;re a real person and helps us keep your account secure.
-          <br />
-          <br />
-          We&apos;ll keep your information private and delete it after
-          we&apos;ve confirmed your identity.
-        </p>
-        <div className="flex items-center p-4 py-4 mx-auto bg-coins-card-bottom max-w-96 rounded-xl gap-4">
-          <span>
-            <LucideInfo className="text-primary-text-dark-pink" size={30} />
-          </span>
-          <p className="text-sm font-medium text-primary-text-dark-pink">
-            Note that these photos will not appear on your profile and are only
-            for verification purposes.
-          </p>
-        </div>
-        <VerificationPageButton />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Verification;
--- src/features/verification/VerificationButton.tsx
+++ src/features/verification/VerificationButton.tsx
@@ -6,41 +6,41 @@ import axiosInstance from "@/utils/Axios";
 import { getToken } from "@/utils/Cookie";
 
 const VerificationPageButton = () => {
-  const handleButtonClick = () => {
-    axiosInstance
-      .post("/verification", {
-        action: "start",
-      })
-      .then((res) => {
-        if (res.data.error) {
-          return toast.error(res.data.message);
-        }
-        swal({
-          title: "Verification Started",
-          text: "You will be redirected to the verification page",
-          icon: "info",
-          buttons: ["Cancel", "Continue"],
-        }).then((value) => {
-          if (value) {
-            window.location.href = `${process.env.NEXT_PUBLIC_VERIFICATION_URL}/${res.data.token}`;
-          }
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        toast.error("An error occurred, please try again later");
-      });
-  };
-  return (
-    <div className="flex flex-col items-center justify-center mt-12">
-      <button
-        onClick={handleButtonClick}
-        className="block px-3 py-3 text-sm font-bold text-center text-white rounded-lg cursor-pointer w-96 bg-primary-dark-pink"
-      >
-        VERIFY
-      </button>
-    </div>
-  );
+    const handleButtonClick = () => {
+        axiosInstance
+            .post("/verification", {
+                action: "start",
+            })
+            .then((res) => {
+                if (res.data.error) {
+                    return toast.error(res.data.message);
+                }
+                swal({
+                    title: "Verification Started",
+                    text: "You will be redirected to the verification page",
+                    icon: "info",
+                    buttons: ["Cancel", "Continue"],
+                }).then((value) => {
+                    if (value) {
+                        window.location.href = `${process.env.NEXT_PUBLIC_VERIFICATION_URL}/${res.data.token}`;
+                    }
+                });
+            })
+            .catch((err) => {
+                console.error(err);
+                toast.error("An error occurred, please try again later");
+            });
+    };
+    return (
+        <div className=" mt-12">
+            <button
+                onClick={handleButtonClick}
+                className="block px-3 py-3 text-sm font-bold text-center text-white rounded-lg cursor-pointer w-full max-w-96 bg-primary-dark-pink"
+            >
+                VERIFY
+            </button>
+        </div>
+    );
 };
 
 export default VerificationPageButton;
